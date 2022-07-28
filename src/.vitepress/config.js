23cf5a30b74d6b5e009a533e8f7fbe0c5b391d8b
Trevor Manz <trevor.j.manz@gmail.com>
2022-07-28 14:56:08 -0400

    feat: add deer emoji

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,4 +1,11 @@
-
+function svgDataHref(emoji) {
+  const svg = `\
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <text y=".9em" font-size="90">${emoji}</text>
+</svg>
+`
+  return "data:image/svg+xml," + encodeURIComponent(svg);
+}
 /**
  * ref: https://vitepress.vuejs.org/config/introduction.html
  *
@@ -7,7 +14,10 @@
 const config = {
   lang: "en-US",
   title: "HMS Bambi",
-  description: "A collection of Harvard Medical School scientists interested in applying our technical skills to support local minority-owned/serving organizations."
+  description: "A collection of Harvard Medical School scientists interested in applying our technical skills to support local minority-owned/serving organizations.",
+  head: [
+    ["link", { rel: "icon", href: svgDataHref('🦌') }]
+  ]
 }
 
 export default config;
